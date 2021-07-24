6f9e95589933c5ecbc37158d298f5e9af355b3a9
Marcos Roberto da Silva <marcos.1998226@gmail.com>
2021-07-24 20:27:30 -0300

    Added method post

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 const app =  express();
 
+app.use(express.json());
+
+app.post('/', (request, response) => {
+    const { message } = request.body;
+    return response.status(200).json({ message })
+});
+
 const PORT = 8080;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
