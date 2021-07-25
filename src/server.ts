4b675febd4d50c0cc116d8800d44f234dad5cef8
Marcos Roberto da Silva <marcos.1998226@gmail.com>
2021-07-25 13:16:49 -0300

    Rising all changes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import "reflect-metadata";
+import "./database";
 
 dotenv.config();
 
@@ -8,11 +10,6 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
-app.post('/', (request, response) => {
-    const { message } = request.body;
-    return response.status(200).json({ message })
-});
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
